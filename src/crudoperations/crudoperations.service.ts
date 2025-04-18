6dc63561b8836f76a7cd4b904a5211219e485c40
unknown <jarunkumar325@gmail.com>
2025-04-18 16:03:25 +0530

    updated

--- src/crudoperations/crudoperations.service.ts
+++ src/crudoperations/crudoperations.service.ts
@@ -1,32 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateCrudoperationDto } from './dto/create-crudoperation.dto';
 import { UpdateCrudoperationDto } from './dto/update-crudoperation.dto';
-import {Crudoperation}  from './entities/crudoperation.entity';
+import { Crudoperation } from './entities/crudoperation.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
 @Injectable()
 export class CrudoperationsService {
-  constructor(@InjectRepository(Crudoperation) 
-  private readonly crudoperationRepository: Repository<Crudoperation>){}
+  constructor(
+    @InjectRepository(Crudoperation)
+    private readonly crudoperationRepository: Repository<Crudoperation>,
+  ) {}
 
-  create(createCrudoperationDto: CreateCrudoperationDto) {
-    return this.crudoperationRepository.save(createCrudoperationDto);
-    
+  async create(createCrudoperationDto: CreateCrudoperationDto): Promise<Crudoperation> {
+    return await this.crudoperationRepository.save(createCrudoperationDto);
   }
 
-  findAll(): Promise<Crudoperation[]> {
-    return this.crudoperationRepository.find();
-  }
-  findOne(id: number) {
-    return `This action returns a #${id} crudoperation`;
+  async findAll(): Promise<Crudoperation[]> {
+    return await this.crudoperationRepository.find();
   }
 
-  update(id: number, updateCrudoperationDto: UpdateCrudoperationDto) {
-    return `This action updates a #${id} crudoperation`;
+  async findOne(id: number): Promise<Crudoperation> {
+    const operation = await this.crudoperationRepository.findOne({ where: { id } });
+    if (!operation) {
+      throw new NotFoundException(`Crudoperation with ID ${id} not found`);
+    }
+    return operation;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} crudoperation`;
+  async update(id: number, updateCrudoperationDto: UpdateCrudoperationDto): Promise<Crudoperation> {
+    const operation = await this.findOne(id); // reuse findOne to check existence
+    const updated = Object.assign(operation, updateCrudoperationDto);
+    return await this.crudoperationRepository.save(updated);
+  }
+
+  async remove(id: number): Promise<string> {
+    const operation = await this.findOne(id); // check existence
+    await this.crudoperationRepository.remove(operation);
+    return `Crudoperation with ID ${id} has been deleted successfully`;
   }
 }
